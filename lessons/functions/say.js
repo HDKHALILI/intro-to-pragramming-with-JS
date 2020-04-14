f714b4947207baa3dde4ebd7a176ad47a17f729e
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-04-14 11:53:57 +1000

    modify lessons/functions/say.js to use function

--- lessons/functions/say.js
+++ lessons/functions/say.js
@@ -1,4 +1,14 @@
-console.log("Hello");
-console.log("hi");
-console.log("how do you do");
-console.log("Quite all right");
+// console.log("Hello");
+// console.log("hi");
+// console.log("how do you do");
+// console.log("Quite all right");
+
+// above code refactored into function
+function say(words) {
+  console.log(words);
+}
+
+say("hello");
+say("hi");
+say("how are you");
+say("I'm fine");
