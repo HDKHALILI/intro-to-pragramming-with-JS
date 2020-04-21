f449e8a6a4fa8fae3bfc171d4f06dced0f9212cf
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-04-21 17:19:00 +1000

    add fibonacci.js

---
+++ lessons/loops-and-iterating/forEach.js
@@ -0,0 +1,16 @@
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+
+// // while loop
+// let index = 0;
+// while (index < names.length) {
+//   console.log(names[index]);
+//   index += 1;
+// }
+// console.log('----------');
+// // for loop
+// for (let index = 0; index < names.length; index += 1) {
+//   console.log(names[index]);
+// }
+
+// we can use forEach method - a built in array iterator method
+names.forEach((name) => console.log(name));
