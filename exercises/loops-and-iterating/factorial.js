3686f85905276936cf18603c80d70f93a0776980
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-04-22 19:25:18 +1000

    use recursion to find factorial, target file: exercises/loops-and-iterating/factorial.js

--- exercises/loops-and-iterating/factorial.js
+++ exercises/loops-and-iterating/factorial.js
@@ -16,12 +16,37 @@ function factorial(num) {
 //   return result;
 // }
 
-console.log(factorial(0));
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(3));
-console.log(factorial(4));
-console.log(factorial(5));
-console.log(factorial(6));
-console.log(factorial(7));
-console.log(factorial(8));
+// solution 3 - using recursion
+function factorialRecursive(num) {
+
+  if (num < 1) {
+    return 1;
+  } else {
+    // times current number with the current number - 1;
+    // when the call reaches the bottom of tree
+    // all the return values are multiplied to gether
+    // like this for factorial of 8: 8 * 7 * 6 * 5 * 4 * 3 * 2
+    return num * factorialRecursive(num - 1);
+  }
+
+
+}
+
+// console.log(factorial(0));
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(5));
+// console.log(factorial(6));
+// console.log(factorial(7));
+// console.log(factorial(8));
+// console.log(factorialRecursive(0));
+// console.log(factorialRecursive(1));
+// console.log(factorialRecursive(2));
+// console.log(factorialRecursive(3));
+// console.log(factorialRecursive(4));
+// console.log(factorialRecursive(5));
+// console.log(factorialRecursive(6));
+// console.log(factorialRecursive(7));
+console.log(factorialRecursive(8));
